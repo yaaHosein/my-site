55ca5fb4837c4c3b2498e85b0c89895ce89bdcea
Yaa_hosein <layali.14.peshawar@example.com>
2023-08-13 17:42:52 +0330

    importing bootstrap

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 import {Aurelia} from 'aurelia-framework';
 import environment from '../config/environment.json';
 import {PLATFORM} from 'aurelia-pal';
+import "bootstrap/dist/css/bootstrap.css";
 
 export function configure(aurelia: Aurelia): void {
   aurelia.use
